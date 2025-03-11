a942fc65c98a04a208875de2f1834202dccdb121
kayware <kay@kayone.pro>
2025-03-11 15:23:29 +0100

    Fixes

--- components/jsy_meter/jsy_meter.h
+++ components/jsy_meter/jsy_meter.h
@@ -4,16 +4,16 @@
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/modbus/modbus.h"
 
-#define SENSOR_SETTER(name) void set_#name#_sensor(sensor_t *s) { this->#name#_sensor_ = s; }
-#define PHASE_SENSOR_SETTER(name) void set_phase_#name#_sensor(uint8_t ph, sensor_t *s)\
-        { this->phases_[ph].#name#_sensor_ = s; }
-#define SENSOR(name) sensor_t *name#_sensor_ { nullptr }
+#define SENSOR_SETTER(name) void set_ ## name ## _sensor(sensor_t *s) { this-> ## name ## _sensor_ = s; }
+#define PHASE_SENSOR_SETTER(name) void set_phase_ ## name ## _sensor(uint8_t ph, sensor_t *s)\
+        { this->phases_[ph]. ## name ## _sensor_ = s; }
+#define SENSOR(name) sensor_t *name ## _sensor_ { nullptr }
 
 namespace esphome {
 namespace jsy_meter {
 
 using sensor_t = sensor::Sensor;
-static const TAG = "jsy_meter";
+static const char *TAG = "jsy_meter";
 
 static const uint8_t MODBUS_CMD_READ = 0x03;
 static const float UNIT_NO_DEC  = 1;
